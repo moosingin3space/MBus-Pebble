d787ac111b654fd6eeb0c4a28843277f65f9b860
Nathan Moos <moosingin3space@gmail.com>
2015-03-06 16:41:12 -0500

    Deleted massive comment

--- src/cumtd.js
+++ src/cumtd.js
@@ -61,54 +61,7 @@ function locationSuccess(pos) {
   // Construct URL
   var url =  "http://pebblebus.herokuapp.com/closest-stop?lat=" + pos.coords.latitude + "&lon=" + pos.coords.longitude;
   // Send request to the CUMTD
-  xhrRequest(url, 'GET',
-    reaction
-             
-             /*function(responseText) {
-      // responseText contains a JSON object with weather info
-      var json = JSON.parse(responseText);
-      
-      // Temperature in Kelvin requires adjustment
-      var stop_id = json.stops[0].stop_id;
-      console.log("Stop ID is " + stop_id);
-   
-      // Conditions
-      var stop_name = json.stops[0].stop_name;
-      console.log("Stop name is " + stop_name);
-      
-      var url2 = "https://developer.cumtd.com/api/v2.2/json/GetDeparturesByStop?key=9a1fc4aa940a4e5384a8950cc476b5e0&stop_id=" + stop_id;
-  
-      // Send request to the CUMTD
-      xhrRequest(url2, 'GET',
-        function(responseText) {
-          // responseText contains a JSON object with weather info
-          var json2 = JSON.parse(responseText);
-
-          var expected = json2.departures[0].expected;
-          console.log("Expected time to arrive: " + expected);
-
-          var headsign = json2.departures[0].headsign;
-          console.log("Trip Headsign is " + headsign);
-          
-          // Assemble dictionary using our keys
-          var dictionary = {
-            "KEY_STOPID": stop_id,
-            "KEY_STOPNAME": stop_name,
-            "KEY_ARRIVAL": expected,
-            "KEY_SIGN": headsign,
-          };
-          
-          // Send to Pebble
-          Pebble.sendAppMessage(dictionary,
-            function(e) {
-              console.log("Stops info sent to Pebble successfully!");
-            },
-            function(e) {
-              console.log("Error sending stops info to Pebble!");
-            });
-        });
-    }*/
-  );
+  xhrRequest(url, 'GET', reaction);
 }
 
 function locationError(err) {
