6e2108d052c6623a2849ae9dcd7da3aa5587f8d4
Nathan Moos <moosingin3space@gmail.com>
2015-04-04 21:12:16 -0400

    Single-endpoint backend

--- src/cumtd.js
+++ src/cumtd.js
@@ -1,66 +1,59 @@
 var xhrRequest = function (url, type, callback) {
   var xhr = new XMLHttpRequest();
   xhr.onload = function () {
-    callback(this.responseText);
+    if (this.status == 200) {
+      callback(this.responseText);
+    } else {
+      xhrRequest(url, type, callback);
+    }
   };
   xhr.open(type, url);
   xhr.send();
 };
 
-function getBus(response, stop_id, stop_name){
-  
-  //get the bus name
-  var json = JSON.parse(response);
-  var busName = json.name;
-  console.log("The next bus that is coming: " + busName);
-  
-    var nextBusTime = json.time;
-    nextBusTime = nextBusTime + " minutes";
-  console.log("Time: " + nextBusTime);
-  //Assemble the information into a dictionary before passing it back to the C
-  var dictionary = {
-            "KEY_STOPID": stop_id,
-            "KEY_STOPNAME": stop_name,
-            "KEY_ARRIVAL": nextBusTime,
-            "KEY_SIGN": busName,
-          };
-          
-          // Send to Pebble
-          Pebble.sendAppMessage(dictionary,
-            function(e) {
-              console.log("Stops info sent to Pebble successfully!");
-            },
-            function(e) {
-              console.log("Error sending stops info to Pebble!");
-            });
-}
 function reaction(response){
      //We are parsing the json
      var json = JSON.parse(response);
      console.log(json);
-     
+  
+     var stop = json[0];
+  
      //collecting the nearest bus stop Id
-     var stop_id = json.id;
+     var stop_id = stop.id;
      console.log('ID: ' + stop_id);
      
      //Collecting the name of that bus stop
-     var stop_name = json.name;
+     var stop_name = stop.name;
      console.log('Name of the bus stop:' + stop_name);
   
-     //Trying to gain access to the details of the incoming bus
-     //Calling the other API
-     var url = "http://pebblebus.appspot.com/next-bus?stop="+ stop_id;
-     
-     //Send request to the API provider
-     xhrRequest(url, 'GET', function(response) {
-       getBus(response, stop_id, stop_name);
-     });
-     
+    //get the bus name
+    var busName = stop.buses[0].name;
+    console.log("The next bus that is coming: " + busName);
+    
+    var nextBusTime = stop.buses[0].time;
+    nextBusTime = nextBusTime + " minutes";
+    console.log("Time: " + nextBusTime);
+    //Assemble the information into a dictionary before passing it back to the C
+    var dictionary = {
+              "KEY_STOPID": stop_id,
+              "KEY_STOPNAME": stop_name,
+              "KEY_ARRIVAL": nextBusTime,
+              "KEY_SIGN": busName,
+            };
+            
+    // Send to Pebble
+    Pebble.sendAppMessage(dictionary,
+                          function(e) {
+                            console.log("Stops info sent to Pebble successfully!");
+                          },
+                          function(e) {
+                            console.log("Error sending stops info to Pebble!");
+                          });
 }
 function locationSuccess(pos) {
   // Construct URL
-  var url =  "http://pebblebus.appspot.com/closest-stop?lat=" + pos.coords.latitude + "&lon=" + pos.coords.longitude;
-  // Send request to the CUMTD
+  var url = "http://pebblebus.appspot.com/closest-stops/@" + pos.coords.latitude + "," + pos.coords.longitude;
+  // Send request to the PebbleBus Backend
   xhrRequest(url, 'GET', reaction);
 }
 
